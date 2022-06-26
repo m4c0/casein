cca6dbc19662b36fb5b1c09a44e7b75c02eb5be9
Eduardo Costa <m4c0@users.noreply.github.com>
2022-06-26 14:43:55 +0100

    Basic event system

---
+++ inc/casein.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace casein {
+  enum event_type { START, REPAINT, QUIT };
+
+  class event {
+    event_type t;
+
+  protected:
+    constexpr event(event_type t) noexcept : t { t } {
+    }
+
+  public:
+    [[nodiscard]] constexpr event_type type() const noexcept {
+      return t;
+    }
+  };
+}
+namespace casein::events {
+  template<event_type ET>
+  class empty_event : public event {
+  public:
+    constexpr empty_event() : event(ET) {
+    }
+  };
+
+  using start = empty_event<START>;
+  using repaint = empty_event<REPAINT>;
+  using quit = empty_event<QUIT>;
+}
+void casein_event(const casein::event & e);
