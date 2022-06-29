2c5d20236844c40bb6c907d42aa6250cdc21a266
Eduardo Costa <m4c0@users.noreply.github.com>
2022-06-29 10:15:54 +0100

    SDL: optional thin wrapper
    SDL is multiplatform and easy to use (as long as you have it in your
    system). Integration is straightforward and self-contained, so it is a
    nice convenience to have here.

---
+++ sdl.cpp
@@ -0,0 +1,47 @@
+#include "casein.hpp"
+#include "casein.sdl.hpp"
+
+#include <SDL.h>
+#include <memory>
+
+[[nodiscard]] static auto & renderer() {
+  static std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> rnd { nullptr, &SDL_DestroyRenderer };
+  return rnd;
+}
+
+static void init_renderer(void * handle) {
+  static std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> wnd { nullptr, &SDL_DestroyWindow };
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    throw std::runtime_error("Failed to initialise SDL");
+  }
+
+  wnd.reset(SDL_CreateWindowFrom(handle));
+  if (!wnd) {
+    throw std::runtime_error("Failed to create SDL window");
+  }
+
+  auto & rnd = renderer();
+  rnd.reset(SDL_CreateRenderer(wnd.get(), -1, 0));
+  if (!rnd) {
+    throw std::runtime_error("Failed to create SDL renderer");
+  }
+}
+
+void casein_event(const casein::event & e) {
+  switch (e.type()) {
+  case casein::CREATE_WINDOW:
+    init_renderer(e.as<casein::events::create_window>().native_window_handle());
+    // Dispatch a new event, but using SDL_Renderer as the new native handle
+    casein_sdl_event(casein::events::create_window { renderer().get() });
+    break;
+  case casein::QUIT:
+    renderer().reset();
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    casein_sdl_event(e);
+    break;
+  default:
+    casein_sdl_event(e);
+    break;
+  }
+}
