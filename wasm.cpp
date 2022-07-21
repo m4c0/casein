b54d6e18bde657357e2f9bbad692d9e51d863822
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-21 18:27:20 +0100

    wasm: linking events

--- wasm.cpp
+++ wasm.cpp
@@ -3,6 +3,15 @@
 // https://github.com/WebAssembly/wasi-sdk
 // https://developer.mozilla.org/en-US/docs/WebAssembly/Using_the_JavaScript_API
 
+extern "C" void casein_key_down() {
+  casein_event(casein::events::key_down { 0 });
+}
+extern "C" void casein_key_up() {
+  casein_event(casein::events::key_up { 0 });
+}
+extern "C" void casein_repaint() {
+  casein_event(casein::events::repaint {});
+}
 int main() {
   casein_event(casein::events::create_window { nullptr });
 }
