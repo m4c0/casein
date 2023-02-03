f9c1b791c49d1981b59c70b49edcd2e8e0e1dd4f
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-03 12:38:49 +0000

    expose only ANativeWindow on android
    let's deal with AAssetManager and others only when needed. This
    simplifies the strongly-typed passage of the native pointer without
    exposing casein to its users

--- android.cpp
+++ android.cpp
@@ -8,14 +8,9 @@ extern "C" void casein_handle(const casein::event &);
 
 static void handle_command(android_app * app, int32_t cmd) {
   switch (cmd) {
-  case APP_CMD_INIT_WINDOW: {
-    auto h = casein::native_handle_t {
-      .asset_manager = app->activity->assetManager,
-      .native_window = app->window,
-    };
-    casein_handle(casein::events::create_window { h });
+  case APP_CMD_INIT_WINDOW:
+    casein_handle(casein::events::create_window { app->window });
     break;
-  }
   case APP_CMD_TERM_WINDOW:
     break;
   case APP_CMD_STOP:
