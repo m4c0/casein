9e62154be94563deb969d09cea7679f8c79d37ef
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-03 10:50:03 +0000

    split build for external refs

--- build.cpp
+++ build.cpp
@@ -1,33 +1,11 @@
-#include "ecow.hpp"
-
-using namespace ecow;
+#include "build.hpp"
 
 int main(int argc, char ** argv) {
+  using namespace ecow;
+  auto m = casein();
+
   seq all { "all" };
-
-  auto m = all.add_unit<per_feat<mod>>("casein");
-
-  m->for_feature(android_ndk).add_unit<>("android");
-
-  m->for_feature(cocoa).add_unit<objc>("osx");
-  m->for_feature(cocoa).add_unit<>("externc");
-
-  m->for_feature(uikit).add_unit<objc>("ios");
-  m->for_feature(uikit).add_unit<>("externc");
-
-  m->for_feature(windows_api).add_unit<>("windows");
-  m->for_feature(windows_api).add_system_library("user32");
-
-  m->for_feature(webassembly).add_unit<>("externc");
-
-  auto mwjs = m->for_feature(webassembly).add_feat<js>();
-  mwjs->set("casein_create_window", "");
-  mwjs->set("casein_repaint", "");
-  mwjs->set("casein_key_down", "");
-  mwjs->set("casein_key_up", "");
-  mwjs->set("casein_mouse_down", "");
-  mwjs->set("casein_mouse_move", "");
-  mwjs->set("casein_mouse_up", "");
+  all.add_ref(m);
 
   auto poc = all.add_unit<app>("poc");
   poc->add_unit<>("poc");
