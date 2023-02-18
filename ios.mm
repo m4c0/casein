d487931329b77f6fef710aa205c1262648b7938d
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-18 09:35:29 +0000

    stuffing ios

--- ios.mm
+++ ios.mm
@@ -4,10 +4,22 @@
 #include "externc.h"
 
 @interface CASAppDelegate : NSObject<UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow * window;
 @end
 
 @implementation CASAppDelegate
 - (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(id)options {
+  MTKView * v = [MTKView new];
+  // TODO: Add delegate
+
+  UIViewController * vc = [UIViewController new];
+  vc.view = v;
+
+  self.window = [UIWindow new];
+  self.window.frame = [UIScreen mainScreen].bounds;
+  self.window.rootViewController = vc;
+  [self.window makeKeyAndVisible];
+
   casein_create_window((__bridge void *)app); // TODO: pass CAMetalLayer
   return YES;
 }
