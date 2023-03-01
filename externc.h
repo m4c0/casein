e72604827d80764ad7ff103d3011033337a44692
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-01 08:48:02 -0300

    fix build on apple platforms

--- externc.h
+++ externc.h
@@ -3,8 +3,8 @@
 
 extern "C" {
 void casein_create_window(void * ptr);
-void casein_key_down(casein::keys key);
-void casein_key_up(casein::keys key);
+void casein_key_down(keys key);
+void casein_key_up(keys key);
 void casein_mouse_down(int btn);
 void casein_mouse_move(int x, int y);
 void casein_mouse_up(int btn);
