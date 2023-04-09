54214714650ffe7a91368522c591efc355bebce7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-09 10:37:57 +0100

    ios: touches

--- ios.mm
+++ ios.mm
@@ -4,6 +4,31 @@
 #include "CASView.h"
 #include "externc.h"
 
+@interface CASViewController : UIViewController
+@end
+
+@implementation CASViewController
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+  UITouch * t = [touches anyObject];
+  CGPoint p = [t locationInView:[self view]];
+  casein_mouse_down(p.x, [self view].frame.size.height - p.y, t.tapCount);
+}
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+  UITouch * t = [touches anyObject];
+  CGPoint p = [t locationInView:[self view]];
+  casein_mouse_up(p.x, self.view.frame.size.height - p.y, t.tapCount);
+}
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+  CGPoint p = [[touches anyObject] locationInView:self.view];
+  casein_mouse_move(p.x, self.view.frame.size.height - p.y);
+}
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+  UITouch * t = [touches anyObject];
+  CGPoint p = [t locationInView:[self view]];
+  casein_mouse_up(p.x, self.view.frame.size.height - p.y, t.tapCount);
+}
+@end
+
 @interface CASAppDelegate : NSObject<UIApplicationDelegate>
 @property (nonatomic, strong) UIWindow * window;
 @end
@@ -11,8 +36,8 @@
 @implementation CASAppDelegate
 - (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(id)options {
 
-  UIViewController * vc = [UIViewController new];
-  vc.view = [CASView new];
+  UIViewController * vc = [CASViewController new];
+  vc.view = [UIView new];
 
   self.window = [UIWindow new];
   self.window.frame = [UIScreen mainScreen].bounds;
