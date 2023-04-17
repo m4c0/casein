67792b27d113414885a1e187c8f394445500e905
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-17 18:26:55 +0100

    ios: dropping "externc"

--- ios.mm
+++ ios.mm
@@ -1,8 +1,10 @@
 @import MetalKit;
 @import UIKit;
 
+using casein_native_handle = CAMetalLayer;
+
 #include "CASView.h"
-#include "externc.h"
+#include "common.hpp"
 
 @interface CASViewController : UIViewController
 @end
@@ -13,27 +15,30 @@
 }
 
 - (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-  casein_gesture(G_SHAKE);
+  casein_handle(casein::events::gesture { casein::G_SHAKE });
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
   UITouch * t = [touches anyObject];
   CGPoint p = [t locationInView:[self view]];
-  casein_mouse_down(p.x,  p.y, t.tapCount - 1);
+  casein_handle(casein::events::mouse_down {
+      { static_cast<int>(p.x), static_cast<int>(p.y), static_cast<int>(t.tapCount) - 1 } });
 }
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
   UITouch * t = [touches anyObject];
   CGPoint p = [t locationInView:[self view]];
-  casein_mouse_up(p.x,  p.y, t.tapCount - 1);
+  casein_handle(
+      casein::events::mouse_up { { static_cast<int>(p.x), static_cast<int>(p.y), static_cast<int>(t.tapCount) - 1 } });
 }
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
   CGPoint p = [[touches anyObject] locationInView:self.view];
-  casein_mouse_move(p.x,  p.y);
+  casein_handle(casein::events::mouse_move { { static_cast<int>(p.x), static_cast<int>(p.y) } });
 }
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
   UITouch * t = [touches anyObject];
   CGPoint p = [t locationInView:[self view]];
-  casein_mouse_up(p.x,  p.y, t.tapCount - 1);
+  casein_handle(
+      casein::events::mouse_up { { static_cast<int>(p.x), static_cast<int>(p.y), static_cast<int>(t.tapCount) - 1 } });
 }
 @end
 
@@ -43,17 +48,18 @@
 
 @implementation CASAppDelegate
 - (void)swipeLeft {
-  casein_gesture(G_SWIPE_LEFT);
+  casein_handle(casein::events::gesture { casein::G_SWIPE_LEFT });
 }
 - (void)swipeRight {
-  casein_gesture(G_SWIPE_RIGHT);
+  casein_handle(casein::events::gesture { casein::G_SWIPE_RIGHT });
 }
 - (void)swipeTop {
-  casein_gesture(G_SWIPE_UP);
+  casein_handle(casein::events::gesture { casein::G_SWIPE_UP });
 }
 - (void)swipeBottom {
-  casein_gesture(G_SWIPE_DOWN);
+  casein_handle(casein::events::gesture { casein::G_SWIPE_DOWN });
 }
+
 - (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(id)options {
   app.applicationSupportsShakeToEdit = YES;
 
@@ -87,7 +93,7 @@
   return YES;
 }
 - (void)applicationWillTerminate:(UIApplication *)app {
-  casein_quit();
+  casein_handle(casein::events::quit {});
 }
 @end
 
