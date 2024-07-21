a921400fc3e418a6fd8426abf84e6d36fe1bc36c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-21 13:08:13 +0100

    moving "extern C" into a impl

--- handler.cpp
+++ handler.cpp
@@ -1,32 +1,6 @@
 module casein;
-import jute;
-import silog;
 
-extern "C" void casein_exit(int);
-void casein::exit(int code) {
-  casein_exit(code);
-}
-
-extern "C" void casein_set_title(const char *);
-void casein::set_title(const char * title) {
-  casein_set_title(title);
-}
-
-extern "C" void casein_enter_fullscreen();
-extern "C" void casein_leave_fullscreen();
-bool in_fullscreen {};
-bool casein::is_fullscreen() {
-  return in_fullscreen;
-}
-void casein::set_fullscreen(bool f) {
-  if (in_fullscreen == f) return;
-  in_fullscreen = f;
-  if (f) {
-    casein_enter_fullscreen();
-  } else {
-    casein_leave_fullscreen();
-  }
-}
+extern "C" void casein_enable_filedrop(bool);
 
 casein::native_handle_t casein::native_ptr;
 dotz::vec2 casein::mouse_pos;
@@ -35,22 +9,6 @@ float casein::screen_scale_factor = 1.0;
 bool casein::keydown_repeating;
 dotz::vec2 casein::window_size;
 hai::varray<hai::cstr> casein::dropped_files {};
-
-extern "C" casein::native_handle_t * casein_native_ptr = &casein::native_ptr;
-extern "C" dotz::vec2 * casein_mouse_pos = &casein::mouse_pos;
-extern "C" dotz::vec2 * casein_mouse_rel = &casein::mouse_rel;
-extern "C" float * casein_screen_scale_factor = &casein::screen_scale_factor;
-extern "C" bool * casein_keydown_repeating = &casein::keydown_repeating;
-extern "C" dotz::vec2 * casein_window_size = &casein::window_size;
-
-extern "C" void casein_enable_filedrop(bool);
-extern "C" void casein_add_drop(const char * str, unsigned len) {
-  jute::view v { str, len };
-  casein::dropped_files.push_back_doubling(v.cstr());
-}
-extern "C" void casein_clear_drops() {
-  casein::dropped_files.truncate(0);
-}
 
 using fn_t = void (*)();
 static fn_t emap[casein::MAX_EVENT_TYPE] {};
