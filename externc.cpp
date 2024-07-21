787b3514ab95c25142e8a7abba1514e2a111c2c7
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-21 13:37:30 +0100

    removing "extern C" layer from exit/fullscreen

--- externc.cpp
+++ externc.cpp
@@ -1,27 +1,6 @@
 module casein;
 import jute;
 
-extern "C" void casein_exit(int);
-void casein::exit(int code) {
-  casein_exit(code);
-}
-
-extern "C" void casein_enter_fullscreen();
-extern "C" void casein_leave_fullscreen();
-static bool in_fullscreen {};
-bool casein::is_fullscreen() {
-  return in_fullscreen;
-}
-void casein::set_fullscreen(bool f) {
-  if (in_fullscreen == f) return;
-  in_fullscreen = f;
-  if (f) {
-    casein_enter_fullscreen();
-  } else {
-    casein_leave_fullscreen();
-  }
-}
-
 extern "C" casein::native_handle_t * casein_native_ptr = &casein::native_ptr;
 extern "C" dotz::vec2 * casein_mouse_pos = &casein::mouse_pos;
 extern "C" dotz::vec2 * casein_mouse_rel = &casein::mouse_rel;
