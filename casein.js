f5828ca28deb856aef456cc2514aff5746dc805d
Eduardo <m4c0@users.noreply.github.com>
2024-08-05 10:28:52 +0100

    wasm: js-side of key processing

---
+++ casein.js
@@ -0,0 +1,4 @@
+(function() {
+  document.addEventListener('keydown', (e) => leco_exports.casein_key(1, e.keyCode));
+  document.addEventListener('keyup', (e) => leco_exports.casein_key(0, e.keyCode));
+})();
