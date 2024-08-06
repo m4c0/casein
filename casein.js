9d300d8101c40dfddf30611430eb16ddae947480
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-06 11:14:22 +0100

    wasm: add a canvas and some wires

--- casein.js
+++ casein.js
@@ -1,4 +1,16 @@
 (function() {
+  const canvas = document.createElement("canvas");
+  canvas.id = "casein-canvas";
+  document.body.appendChild(canvas);
+
   document.addEventListener('keydown', (e) => leco_exports.casein_key(1, e.keyCode));
   document.addEventListener('keyup', (e) => leco_exports.casein_key(0, e.keyCode));
+
+  leco_imports.casein = {
+    window_size : (x, y) => {
+      canvas.style.width = x;
+      canvas.style.height = y;
+    },
+    window_title : (x, y) => console.log(x, y),
+  };
 })();
