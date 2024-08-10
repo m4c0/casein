003c48c83582df48551bb6ff2f4bb0d343766dd8
Eduardo <m4c0@users.noreply.github.com>
2024-08-10 11:23:14 +0100

    js: centralising canvas

--- casein.js
+++ casein.js
@@ -1,7 +1,13 @@
-(function() {
+!function() {
   const canvas = document.createElement("canvas");
   canvas.id = "casein-canvas";
   canvas.style.border = "1px solid black";
+  canvas.style.position = "absolute";
+  canvas.style.top = "50%";
+  canvas.style.left = "50%";
+  canvas.style.transform = "translate(-50%, -50%)";
+
+  document.body.style.position = "relative";
   document.body.appendChild(canvas);
 
   document.addEventListener('keydown', (e) => leco_exports.casein_key(1, e.keyCode));
@@ -14,4 +20,4 @@
     },
     window_title : (ptr, size) => document.title = vaselin_tostr(ptr, size),
   };
-})();
+}();
